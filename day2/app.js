06f2aa84e16091d7edd00243d80bcfadcfca8795
Ling-I Huang <liondonaugust810810@gmail.com>
2020-12-26 00:55:13 -0500

    add read-query-string

--- day2/app.js
+++ day2/app.js
@@ -16,7 +16,11 @@ const server = http.createServer((req, res) => {
   // res.writeHead(200, {'Content-Type': 'text/html'});
 
   // write a response to the client
-  res.write('<h1>Hello World! The date and time are currently: </h1>' + dt.myDateTime());
+  res.write(`<h1>Hello World!</h1>`)
+  res.write(`<h2>1. The date and time are currently: ${dt.myDateTime()}</h2>`);
+
+  // read the query string
+  res.write(`<h2>2. The query string is: ${req.url}</h2>`)
 
   // end the response
   res.end();
