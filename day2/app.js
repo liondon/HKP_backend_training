6377001f8e16bafb2056cf70140224a6a51648e6
Ling-I Huang <liondonaugust810810@gmail.com>
2020-12-26 00:49:16 -0500

    chore: add code comment

--- day2/app.js
+++ day2/app.js
@@ -1,21 +1,28 @@
 /* This is adopted from w3schools.com - Node.js Tutorial: https://www.w3schools.com/nodejs/default.asp */
 
-// include the `http` module
+// include the modules
 const http = require('http');
 const dt = require('./module')
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// create Node.js server
 const server = http.createServer((req, res) => {
+  // add HTTP Header
   res.statusCode = 200
   res.setHeader('Content-type', 'text/html');
-  // can also write these in one line:
+  // can also do this in one line:
   // res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // write a response to the client
   res.write('<h1>Hello World! The date and time are currently: </h1>' + dt.myDateTime());
+
+  // end the response
   res.end();
 });
 
+// the server object listens on `hostname:port`
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
