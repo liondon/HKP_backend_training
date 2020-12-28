6ecbd9453564f0527420b3d56f581e91669e2f96
Ling-I Huang <liondonaugust810810@gmail.com>
2020-12-28 01:32:58 -0500

    refactor with async/await syntax

--- day3/app.js
+++ day3/app.js
@@ -10,58 +10,46 @@ MongoClient.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(db => {
-    const dbo = db.db('mydb')
-    console.log('Database created!')
+  .then(async db => {
+    try {
+      const dbo = db.db('mydb')
+      console.log('Database created!')
 
-    // // 2. create a collection
-    // // NOTE: throw error when the collection already exist!
-    // // NOTE: In MongoDB, a collection is not created until it gets content! MongoDB waits until you have inserted a document before it actually creates the collection.
-    // dbo.createCollection('customers')
-    //   .then(res => {
-    //     console.log('Collection created!')
-    //   })
+      // // 2. create a collection
+      // // NOTE: throw error when the collection already exist!
+      // // NOTE: In MongoDB, a collection is not created until it gets content! MongoDB waits until you have inserted a document before it actually creates the collection.
+      // await dbo.createCollection('customers')
+      // console.log('Collection created!')
 
-    // 3.1 insert a record
-    // NOTE: If you try to insert documents in a collection that do not exist, MongoDB will create the collection automatically.
-    const record = { name: 'Co Inc', address: 'Highway 37' }
-    dbo.collection('customers')
-      .insertOne(record)
-      .then(res => {
-        console.log('a single document inserted!')
+      // 3.1 insert a record
+      // NOTE: If you try to insert documents in a collection that do not exist, MongoDB will create the collection automatically.
+      const record = { name: 'Co Inc', address: 'Highway 37' }
+      await dbo.collection('customers').insertOne(record)
+      console.log('a single document inserted!')
 
-        // 3.2 insert many records
-        const records = [
-          { name: 'name_1', address: 'addr_1' },
-          { name: 'name_2', address: 'addr_2' }
-        ]
-        dbo.collection('customers')
-          .insertMany(records)
-          .then(res => {
-            console.log('many documents inserted! #Documents inserted = ' + res.insertedCount)
+      // 3.2 insert many records
+      const records = [
+        { name: 'name_1', address: 'addr_1' },
+        { name: 'name_2', address: 'addr_2' }
+      ]
+      const res = await dbo.collection('customers').insertMany(records)
+      console.log('many documents inserted! #Documents inserted = ' + res.insertedCount)
+      // 3.3 take a look at the result obj
+      console.log(JSON.stringify(res))
 
-            // 3.3 take a look at the result obj
-            console.log(JSON.stringify(res))
+      // 4.1 find one record that matches query cond
+      let result = await dbo.collection('customers').findOne({})
+      console.log(result.name)
 
-            // 4.1 find one record that matches query cond
-            dbo.collection('customers')
-              .findOne({})
-              .then(result => {
-                console.log(result.name)
+      // 4.2 find all records that matches query cond
+      result = await dbo.collection('customers').find({}).toArray()
+      console.log(result)
 
-                // 4.2 find all records that matches query cond
-                dbo.collection('customers')
-                  .find({})
-                  .toArray()
-                  .then(result => {
-                    console.log(result)
-
-                    // finally, close the connection
-                    console.log('Closing the connection!')
-                    db.close()
-                  })
-              })
-          })
-      })
+      // finally, close the connection
+      console.log('Closing the connection!')
+      db.close()
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
   })
-  .catch(err => { throw err })
