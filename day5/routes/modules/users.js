10fb0c5b392498b0bf5acfb646d1ef66f0d084c8
Ling-I Huang <liondonaugust810810@gmail.com>
2020-12-30 03:01:28 -0500

    return JWT directly for successful signup and refine error handling

--- day5/routes/modules/users.js
+++ day5/routes/modules/users.js
@@ -6,22 +6,6 @@ const router = express.Router()
 const User = require('../../models/user')
 const secret = 'Put the Secret in .env!!!'
 
-router.post('/create', async (req, res) => {
-  /* Creates a User
-   * This endpoint creates(signup) a User based the data in the body that is posted
-   */
-  const message = {}
-  const { username, password } = req.body
-
-  // [INFO] log the request body
-  // TODO: is there a better logger that can handle different tags?
-  console.log('Request to create(signup) user with following payload: ')
-  console.log(req.body)
-
-  // check empty fileds (should/could be done at frontend)
-  if (!username || !password) {
-    message.Error = 'Some required fileds are missing!'
-    return res.status(400).json(message)
 router.post('/login', async (req, res) => {
   try {
     const message = {}
@@ -44,22 +28,46 @@ router.post('/login', async (req, res) => {
   }
 })
 
-  // check if the username has been used
-  const existingUser = await User.findOne({ username })
-  if (existingUser) {
-    message.Error = 'This username has been registered. Please try another one.'
-    return res.status(400).json(message)
-  }
+// TODO: confirm the convention of docstring
+/* Creates a User
+ * This endpoint creates(signup) a User based the data in the body that is posted */
+router.post('/create', async (req, res) => {
+  try {
+    const message = {}
+    const { username, password } = req.body
 
-  // create new user
-  const hash = await bcrypt.hashSync(password, await bcrypt.genSaltSync(10))
-  const user = await User.create({
-    username,
-    password: hash
-  })
-  console.log(`User with ID ${user._id} created.`)
-  message.Location = `http://localhost:3000/users/${user._id}`
-  return res.status(201).json(message)
+    // [INFO] log the request body
+    // TODO: is there a better logger that can handle different tags?
+    console.log('Request to create(signup) user with following payload: ')
+    console.log(req.body)
+
+    // check empty fileds (should/could be done at frontend)
+    if (!username || !password) {
+      message.message = 'Error: Some required fileds are missing!'
+      return res.status(400).json(message)
+    }
+
+    // check if the username has been used
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+      message.message = 'Error: This username has been registered. Please try another one.'
+      return res.status(400).json(message)
+    }
+
+    // create new user
+    const hash = await bcrypt.hashSync(password, await bcrypt.genSaltSync(10))
+    const user = await User.create({
+      username,
+      password: hash
+    })
+    console.log(`User with ID ${user._id} created.`)
+    message.Location = `http://localhost:3000/users/${user._id}`
+    message.token = jwt.sign({ username }, secret, { expiresIn: '1d' })
+    return res.status(201).json(message)
+  } catch (err) {
+    console.log(err)
+    // TODO: what should be returned? 500?
+  }
 })
 
 module.exports = router
