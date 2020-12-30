2327a0af50686f92a7a0bd8ae15deb6b81970e28
Ling-I Huang <liondonaugust810810@gmail.com>
2020-12-30 03:12:56 -0500

    chore: add comments

--- day5/routes/modules/users.js
+++ day5/routes/modules/users.js
@@ -4,22 +4,37 @@ const jwt = require('jsonwebtoken')
 const router = express.Router()
 
 const User = require('../../models/user')
-const secret = 'Put the Secret in .env!!!'
+const secret = 'Put the Secret in .env!!!' // TODO: incloud dotenv
 
+// TODO: confirm the convention of docstring
+/* Login a User
+ * This endpoint login a User based the data in the body that is posted */
 router.post('/login', async (req, res) => {
   try {
     const message = {}
     const { username, password } = req.body
+
+    // [Error handling] check empty fields (should/could be done at frontend)
+    if (!username || !password) {
+      message.message = 'Error: Some required fields are missing!'
+      return res.status(400).json(message)
+    }
+
+    // [Error handling] get user data from database & check if user exist
     const user = await User.findOne({ username })
     if (!user) {
       message.message = 'Error: username not found.'
       return res.status(400).json(message)
     }
+
+    // [Error handling] check password
     const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
       message.message = 'Error: Wrong password.'
       return res.status(400).json(message)
     }
+
+    // assign token
     message.token = jwt.sign({ username }, secret, { expiresIn: '1d' })
     res.status(200).json(message)
   } catch (err) {
@@ -36,18 +51,18 @@ router.post('/create', async (req, res) => {
     const message = {}
     const { username, password } = req.body
 
-    // [INFO] log the request body
+    // [LOG-INFO] log the request body for debugging
     // TODO: is there a better logger that can handle different tags?
     console.log('Request to create(signup) user with following payload: ')
     console.log(req.body)
 
-    // check empty fileds (should/could be done at frontend)
+    // [Error handling] check empty fields (should/could be done at frontend)
     if (!username || !password) {
-      message.message = 'Error: Some required fileds are missing!'
+      message.message = 'Error: Some required fields are missing!'
       return res.status(400).json(message)
     }
 
-    // check if the username has been used
+    // [Error handling] check if the username has been used
     const existingUser = await User.findOne({ username })
     if (existingUser) {
       message.message = 'Error: This username has been registered. Please try another one.'
