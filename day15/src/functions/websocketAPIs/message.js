af7abfb936defdfb88c1ee5140451b19bd4a90ad
Ling-I Huang <liondonaugust810810@gmail.com>
2021-01-09 00:53:38 -0500

    add websocket-message function

---
+++ day15/src/functions/websocketAPIs/message.js
@@ -0,0 +1,30 @@
+const Response = require('../utils/API_Responses')
+const DynamoDB = require('../../../../day14/src/functions/utils/DynamoDB')
+
+const tableName = process.env.tableName
+
+exports.handler = async event => {
+  console.log('event', event)
+
+  const { connectionId: connectionID } = event.requestContext
+
+  const body = JSON.parse(event.body)
+
+  try {
+    const record = await DynamoDB.get(connectionID, tableName)
+    const messages = record.messages
+
+    messages.push(body.message)
+
+    const data = {
+      ...record,
+      messages
+    }
+
+    await DynamoDB.write(data, tableName)
+    return Response._200({ message: 'got a message' })
+  } catch (err) {
+    console.log('Error when receiving message', err)
+    return Response._400({ message: 'message not received' })
+  }
+}
