246a45e6919363111917aa8db731d23fc664d5fc
Ling-I Huang <liondonaugust810810@gmail.com>
2021-01-24 00:33:45 -0500

    add-default-hooks

--- day14-sls2/src/functions/createPlayerScore.js
+++ day14-sls2/src/functions/createPlayerScore.js
@@ -1,25 +1,26 @@
 const Responses = require('../functions/utils/API_Responses.js')
 const DynamoDB = require('../functions/utils/DynamoDB')
+const withHooks = require('../functions/utils/hooks')
+
 const tableName = process.env.tableName
 
-exports.handler = async event => {
-  console.log('event', event)
-  if (!event.pathParameters || !event.pathParameters.ID) {
+const handler = async event => {
+  if (!event.pathParameters.ID) {
     // failed without an ID... This path is not reached, actually.
     return Responses._400({ message: 'missing the ID query parameter' })
   }
 
   const ID = event.pathParameters.ID
-  const data = JSON.parse(event.body)
+  const data = event.body
   data.ID = ID
 
-  try {
-    const user = await DynamoDB.create(data, tableName)
-    return Responses._200(user)
-  } catch (err) {
-    console.log('Error in DynamoDB create', err)
+  const user = await DynamoDB.create(data, tableName)
+  if (!user) {
     return Responses._400({
       message: `Failed to create user by ID=${ID}`
     })
   }
+  return Responses._200(user)
 }
+
+exports.handler = withHooks(handler)
